84a3df519cb8d3f0a3a9070ad6ae3f90c8b78b31
Daniel P <stacatogit@gmail.com>
2015-12-10 06:26:35 +0100

    Initial Code Commit
    This is the initial Code Commit for the Project
    Includes:
    - A Base Extraction Project
    - A concrete Extraction Project for NUnit Test DLLs
    - A Base Structure Project specifying how Tests Classes and Test Methods
    are set up
    - A simple WPF UI that allows loading in a handfull of Test DLLs and
    extracts the Data by Test Suit or Test Methods

---
+++ TestExtractor/Extractors/NUnit/Structure/StubNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Core;
+using TestExtractor.Structure;
+
+namespace TestExtractor.Extractors.NUnit.Structure
+{
+    /// <summary>
+    ///     Concrete implementation of the Stub Node Interface
+    ///     Class :     <see cref="Node" />
+    ///     Interface : <see cref="IStubNode" />
+    /// </summary>
+    [Serializable]
+    public class StubNode : Node, IStubNode
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="StubNode" /> class.
+        /// </summary>
+        /// <param name="node">NUnit Test Node</param>
+        public StubNode (TestNode node)
+            : base(node)
+        {
+        }
+
+        #region Implementations of <see cref="ITestMethodNode" />
+
+        /// <summary>
+        ///     Implements <see cref="IStubNode.StubName" />
+        /// </summary>
+        public string StubName
+        {
+            get
+            {
+                return Tnode.MethodName;
+            }
+        }
+
+        #endregion
+
+        #region IEquatable
+
+        /// <summary>
+        ///     Implements <see cref="IEquatable{T}.Equals(T)" />
+        /// </summary>
+        public bool Equals (IStubNode other)
+        {
+            return base.Equals(other);
+        }
+
+        #endregion
+    }
+}
---
+++ TestExtractor/Structure/IStubNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestExtractor.Structure
+{
+    /// <summary>
+    ///     Interace specifying the API of a Stub Node
+    ///     Interface : <see cref="INode" />
+    ///     Interface : <see cref="IEquatable{T}" />
+    /// </summary>
+    public interface IStubNode : INode, IEquatable<IStubNode>
+    {
+        /// <summary>
+        ///     Name of the Stub Node
+        /// </summary>
+        string StubName { get; }
+    }
+}
